3e98dcb0cdd8cb7bbd2ddb090da9d61b7d7bf86c
= <=>
2016-11-28 13:38:45 +1100

    Removed unneccesary lifetime specifier

--- src/generic.rs
+++ src/generic.rs
@@ -8,6 +8,6 @@ pub trait TurnPlan: Clone {}
 ///TODO: MOVE THIS TO ANOTHER/MORE GENERIC CRATE ALONG WITH SOME OTHER STUFF AS A GENERIC BASE READY FOR FANTASTIC BITS
 ///empty atm supposed to be a base type for AIs such that they can verse themselves or a simpler enemys
 
-pub trait AI<'a, Info,State,Plan>where Info: GameInfo, State: GameState, Plan: TurnPlan {
+pub trait AI<Info,State,Plan>where Info: GameInfo, State: GameState, Plan: TurnPlan {
 	fn generate_action(&mut self, state: & State) -> Plan;
 }
